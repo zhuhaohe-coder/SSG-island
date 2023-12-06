9f2980a0938fae29d3edb2fd340d13e7f652bbd7
Haohe Zhu <2452466220@qq.com>
2023-12-06 20:25:15 +0800

    初始化主题组件+热更新

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
-import { createServer as createViteServer } from "vite";
+import { createServer } from "vite";
 import { pluginIndexHtml } from "./plugin-island/indexHtml";
+import pluginReact from "@vitejs/plugin-react";
 
-export async function createDevServer(root = process.cwd()) {
-  return createViteServer({
+export function createDevServer(root: string) {
+  return createServer({
     root,
-    plugins: [pluginIndexHtml()],
+    plugins: [pluginIndexHtml(), pluginReact()],
   });
 }
--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,23 +1,43 @@
 import { readFile } from "fs/promises";
 import { Plugin } from "vite";
-import { DEFAULT_HTML_PATH } from "../constants";
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from "../constants";
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: "island:index-html",
     apply: "serve",
+    // 插入入口 script 标签
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: "body",
+          },
+        ],
+      };
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
-          //1. 读取template.html的内容
-          const content = await readFile(DEFAULT_HTML_PATH, "utf-8");
+          let html = await readFile(DEFAULT_HTML_PATH, "utf-8");
+
           try {
-            //2. 响应HTML浏览器
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl
+            );
             res.statusCode = 200;
             res.setHeader("Content-Type", "text/html");
-            res.end(content);
-          } catch (error) {
-            return next(error);
+            res.end(html);
+          } catch (e) {
+            return next(e);
           }
         });
       };

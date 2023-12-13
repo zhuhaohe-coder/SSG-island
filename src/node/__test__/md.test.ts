28b7a36d79436409b30cca6707997e2fb5d5bdf4
Haohe Zhu <2452466220@qq.com>
2023-12-13 15:40:50 +0800

    feat: rehype-plugin 代码块

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -3,13 +3,15 @@ import { describe, test, expect } from 'vitest';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import { rehypePluginPreWrapper } from '../plugin-mdx/rehypePlugins/preWrapper';
 
 describe('Markdown compile cases', () => {
   // 初始化 processor
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeStringify);
+    .use(rehypeStringify)
+    .use(rehypePluginPreWrapper);
 
   test('Compile title', async () => {
     const mdContent = '# 123';
@@ -18,7 +20,7 @@ describe('Markdown compile cases', () => {
   });
 
   test('Compile code', async () => {
-    const mdContent = 'I am using `Island.js`';
+    const mdContent = '```js\n console.log(123);\n```';
     const result = processor.processSync(mdContent);
     expect(result.value).toMatchInlineSnapshot();
   });
---
+++ src/node/plugin-mdx/rehypePlugins/preWrapper.ts
@@ -0,0 +1,59 @@
+import type { Plugin } from 'unified';
+import { visit } from 'unist-util-visit';
+import type { Element as HastElement, Root } from 'hast';
+interface Element extends HastElement {
+  data?: {
+    isVisited?: boolean;
+  };
+}
+
+export const rehypePluginPreWrapper: Plugin<[], Root> = () => {
+  return (tree) => {
+    visit(tree, 'element', (node: Element) => {
+      // 1. 找到 pre 元素
+      if (
+        node.tagName === 'pre' &&
+        node.children[0]?.type === 'element' &&
+        node.children[0].tagName === 'code' &&
+        !node.data?.isVisited
+      ) {
+        // 2. 解析出代码的语言名称
+        const codeNode = node.children[0];
+        const codeClassName = codeNode.properties?.className?.toString() || '';
+        //<pre><code class="language-js"> console.log(123)</code></pre>;
+        const lang = codeClassName.split('-')[1];
+        // 3. 变换 Html AST
+        const cloneNode: Element = {
+          type: 'element',
+          tagName: 'pre',
+          children: node.children,
+          data: {
+            isVisited: true
+          },
+          properties: {}
+        };
+        // 修改原来的 pre 标签 -> div 标签
+        node.tagName = 'div';
+        node.properties = node.properties || {};
+        node.properties.className = codeClassName;
+        // 构造 div 标签的子元素
+        node.children = [
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: 'lang'
+            },
+            children: [
+              {
+                type: 'text',
+                value: lang
+              }
+            ]
+          },
+          cloneNode
+        ];
+      }
+    });
+  };
+};
